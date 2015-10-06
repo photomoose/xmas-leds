aab869aae455c3dacdb5d358eb8ab3b9aab4e66d
tom.davis <tom.davis@photomoose.com>
2015-10-06 07:37:54 +0100

    Created WebAPI project.

---
+++ XmasLeds/XmasLeds.WebApi/Controllers/LedsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+
+namespace XmasLeds.WebApi.Controllers
+{
+    public class LedsController : ApiController
+    {
+        private MessagingFactory _factory;
+
+        public LedsController()
+        {
+            var connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            _factory = MessagingFactory.CreateFromConnectionString(connectionString);
+        }
+
+        public IHttpActionResult Post([FromBody]ColourRequest colourRequest)
+        {
+            if (colourRequest == null)
+            {
+                return BadRequest("Invalid colour request.");
+            }
+
+            var topicClient = _factory.CreateTopicClient("Commands");
+
+            var json = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(colourRequest)));
+            var brokeredMessage = new BrokeredMessage(json);
+
+            topicClient.Send(brokeredMessage);
+
+            return Ok();
+        }
+    }
+
+    public class ColourRequest
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+    }
+}
---
+++ XmasLeds/XmasLeds.WebApi/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace XmasLeds.WebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+            var td = new TopicDescription("Commands");
+
+            if (!namespaceManager.TopicExists(td.Path))
+            {
+                namespaceManager.CreateTopic(td);
+            }
+        }
+    }
+}
