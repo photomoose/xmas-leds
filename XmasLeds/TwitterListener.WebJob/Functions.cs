17996fc494c87d72207cc4d54ec79cc1a340df5f
tom.davis <tom.davis@photomoose.com>
2015-10-07 22:46:12 +0100

    Created Web Job.

---
+++ XmasLeds/TwitterListener.WebJob/Functions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using Tweetinvi;
+using Tweetinvi.Core.Credentials;
+using Tweetinvi.Core.Events.EventArguments;
+using Stream = Tweetinvi.Stream;
+
+namespace TwitterListener.WebJob
+{
+    public class Functions
+    {
+        private static MessagingFactory _factory;
+
+        [NoAutomaticTrigger]
+        public async static void ListenForTweetsAsync(TextWriter log)
+        {
+            var connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            _factory = MessagingFactory.CreateFromConnectionString(connectionString);
+            
+            var credentials = new TwitterCredentials(
+                "51Xm7YboVAcVIPB6ppBNcCeGC",
+                "VCfto1iyxzrAEzyr1vG3AbnqWgMB4SJRyWm6itiVaHuKqGXKn9",
+                "3881153422-w1gNQ0NsOYR4l84VHVyvI8RnRhtfu6fBz8uoRmR",
+                "dV2ORE1gx3zf5ohvvCtvKDmMLyWsrfRmftI1bsIKNUfdZ");
+
+            Auth.SetCredentials(credentials);
+
+            var stream = Stream.CreateUserStream();
+            stream.TweetCreatedByAnyoneButMe += OnTweetCreated;
+            
+            await stream.StartStreamAsync();
+        }
+
+        private async static void OnTweetCreated(object sender, TweetReceivedEventArgs e)
+        {
+            await Console.Out.WriteLineAsync("Received tweet: " + e.Tweet);
+
+            var matches = Regex.Match(e.Tweet.Text, "(\\d{1,3}),(\\d{1,3}),(\\d{1,3})", RegexOptions.Compiled);
+
+            if (matches.Success)
+            {
+                var colourRequest = new ColourRequest
+                {
+                    Red = int.Parse(matches.Groups[1].Value),
+                    Green = int.Parse(matches.Groups[2].Value),
+                    Blue = int.Parse(matches.Groups[3].Value)
+                };
+
+                var topicClient = _factory.CreateTopicClient("Commands");
+
+                var json = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(colourRequest)));
+                var brokeredMessage = new BrokeredMessage(json);
+
+                topicClient.Send(brokeredMessage);
+
+                Tweet.PublishTweetInReplyTo(string.Format("@{0} Thanks for your request!", e.Tweet.CreatedBy.ScreenName), e.Tweet);
+            }
+        }
+    }
+
+    public class ColourRequest
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+    }
+}
---
+++ XmasLeds/XmasLeds.WebApi/Controllers/SmsController.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+
+namespace XmasLeds.WebApi.Controllers
+{
+    public class SmsController : Controller
+    {
+        private MessagingFactory _factory;
+
+        public SmsController()
+        {
+            var connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            _factory = MessagingFactory.CreateFromConnectionString(connectionString);            
+        }
+
+        [HttpPost]
+        public ActionResult Index(string from, string body)
+        {
+            var matches = Regex.Match(body, "^(\\d{1,3}),(\\d{1,3}),(\\d{1,3})$", RegexOptions.Compiled);
+
+            if (matches.Success)
+            {
+                var colourRequest = new ColourRequest
+                {
+                    Red = int.Parse(matches.Groups[1].Value),
+                    Green = int.Parse(matches.Groups[2].Value),
+                    Blue = int.Parse(matches.Groups[3].Value)
+                };
+
+                var topicClient = _factory.CreateTopicClient("Commands");
+
+                var json = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(colourRequest)));
+                var brokeredMessage = new BrokeredMessage(json);
+
+                topicClient.Send(brokeredMessage);
+
+                var response = new TwilioResponse();
+                response.Message(string.Format("Hello {0}. Your colour request was successful.", from));
+
+                return new TwiMLResult(response);
+            }
+            else
+            {
+                var response = new TwilioResponse();
+                response.Message(string.Format("Hello {0}. Your msg was {1}. Have a nice day!", from, body));
+
+                return new TwiMLResult(response);
+            }
+        }
+    }
+}
