2737453640556b9eea506152dc594681ac62c1e7
tom.davis <tom.davis@photomoose.com>
2015-10-16 18:09:51 +0100

    Cleaned up tests.

--- XmasLeds/DurryLights.Domain/LightDisplayParser.cs
+++ XmasLeds/DurryLights.Domain/LightDisplayParser.cs
@@ -16,62 +16,28 @@ namespace Rumr.DurryLights.Domain
 
         public async Task<LightDisplay> ParseAsync(string text)
         {
-            var matches = Regex.Match(text, "(\\d{1,3}), ?(\\d{1,3}), ?(\\d{1,3})", RegexOptions.Compiled);
-
-            if (matches.Success)
-            {
-                var colour = new Colour(
-                    "Custom",
-                    byte.Parse(matches.Groups[1].Value),
-                    byte.Parse(matches.Groups[2].Value),
-                    byte.Parse(matches.Groups[3].Value));
-
-                return new LightDisplay(new[] {colour});
-            }
-            else
-            {
-                text = StripTwitterUsernames(text);
-
-                var potentialColours = SplitIntoWords(text)
-                                        .Select(c => c.ToLower())
-                                        .ToList();
+            var potentialColours = SplitIntoWords(text)
+                                    .Select(c => c.ToLower())
+                                    .ToList();
                 
-                var matchingColours = (await _colourRepository.FindColoursAsync(potentialColours.Distinct())).ToDictionary(c => c.Name);
+            var matchingColours = (await _colourRepository.FindColoursAsync(potentialColours.Distinct())).ToDictionary(c => c.Name);
 
-                var matchedColours = new List<Colour>();
+            var matchedColours = new List<Colour>();
                 
-                foreach (var potentialColour in potentialColours)
+            foreach (var potentialColour in potentialColours)
+            {
+                if (matchingColours.ContainsKey(potentialColour))
                 {
-                    if (matchingColours.ContainsKey(potentialColour))
-                    {
-                        matchedColours.Add(matchingColours[potentialColour]);
-                    }
-                }
-
-                if (matchedColours.Any())
-                {
-                    return new LightDisplay(matchedColours);
-                }
-                else
-                {
-                    return null;
+                    matchedColours.Add(matchingColours[potentialColour]);
                 }
             }
-        }
 
-        private string StripTwitterUsernames(string text)
-        {
-            return Regex.Replace(text, "@(\\w){1,15}\\s+", string.Empty);
+            return matchedColours.Any() ? new LightDisplay(matchedColours) : null;
         }
 
         private static IEnumerable<string> SplitIntoWords(string text)
         {
             return Regex.Split(text, ",\\s*");
-        }
-
-        private static bool IsNotATwitterUsername(string text)
-        {
-            return !text.StartsWith("@");
         }
     }
 }
